22e2610f197af0ff66583cd1d51f86de2fc2657e
Nak0_x <nak0x@proton.me>
2026-06-22 15:45:20 +0200

    Feat: change the s3 buckt towards the school shit

--- main/app/utils/assetPath.js
+++ main/app/utils/assetPath.js
@@ -1,4 +1,4 @@
-const BASE = import.meta.env.VITE_ASSETS_BASE_URL ?? 'https://minio.mycloud-anthony.ovh/assets'
+const BASE = import.meta.env.VITE_ASSETS_BASE_URL ?? 'https://s3-vmgddq43sii69fcmt69l5639.gobelinsannecy.fr/project-adhoc/'
 
 export function assetPath(path) {
   return BASE + path
